7014b51daa17d32cd986a8614456dd66c37819ae
karolmakowski <163434686+karolmakowski@users.noreply.github.com>
2024-04-16 18:51:32 +0200

    wip: add new menu function, add user search

--- models/data_source.py
+++ models/data_source.py
@@ -2,6 +2,6 @@ users: list = [
 
     {"name": "Julia", "surname": "Gotowiec", "posts": 1500, },
     {"name": "Hubert", "surname": "Sybilski", "posts": 123456, },
-    {"name": "Adrian", "surname": "Dobrzański", "posts": 3, }
-
+    {"name": "Adrian", "surname": "Dobrzański", "posts": 3, },
+    {"name": "Bartek", "surname": "Wyrzykowski", "posts": 666, }
 ]
